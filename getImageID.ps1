7ccdfaa804fa6a91f4953e43c9c3b78722dc23cf
Brandon Spell <brandon.spell@rackspace.com>
2021-09-08 09:02:13 -0500

    add more ami searches

--- getImageID.ps1
+++ getImageID.ps1
@@ -19,26 +19,42 @@ param (
     [string]
     $Region,
 
-    [ValidateSet('AmazonLinux2')]
-    $AMI = 'AmazonLinux2'
+    [ValidateSet('AmazonLinux2','Ubuntu20','Windows2019')]
+    $AMI
 )
 Process {
-    #ignore this for now
-    $amidescriptionfilter = switch ( $AMI ) {
-        AmazonLinux2 { 'Amazon Linux 2 AMI 2.0.*HVM gp2' }
-        Default { 'Amazon Linux 2 AMI 2.0.*HVM gp2'}
-    }
     $amiSplat = @{
         Region = $Region
         Filter = @(
-            @{Name = 'state';Values = 'available'},
-            @{Name = 'owner-alias';Values = 'amazon'},
-            @{Name="description"; Values=$amidescriptionfilter}
+            @{
+                Name = 'state';
+                Values = 'available'
+            },
+            @{
+                Name = 'architecture'
+                Values = 'x86_64'
+            }
         )
+    }
+    #ignore this for now
+    switch ( $AMI ) {
+        AmazonLinux2 { 
+            $amiSplat.Filter += @{Name = 'owner-id';Values = '137112412989'}
+            $amiSplat.Filter += @{Name="description"; Values='Amazon Linux 2 AMI 2.0.*HVM gp2'}
+             }
+        Ubuntu20 {
+            $amiSplat.Filter += @{Name = 'owner-id';Values = '099720109477'}
+            $amiSplat.Filter += @{Name='description'; Values='Canonical, Ubuntu, 20.04 LTS, amd64 focal image build on*'}
+        }
+        Windows2019 {
+            $amiSplat.Filter += @{Name = 'name';Values= 'Windows_Server-2019-English-Full-Base-*'}
+            $amiSplat.Filter += @{Name = 'owner-id';Values = '801119661308'}
+        }
+        Default { return 'Specify an OS Bro[|Bro-ette]!'}
     }
     $images = Get-EC2Image @amiSplat
 
     $image = $images | Sort-Object CreationDate -Descending | Select-Object -First 1
 
-    return $image.ImageId
+    $image | select Name,Description,ImageId,CreationDate,OwnerId
 }
